c6db91026e726b778bf00f4b6cc83e972b377483
アリス <alice@rootsys.sh>
2024-01-27 23:28:45 +0000

    The rest of the tracker

--- scripts/autotracking/ap_helper.lua
+++ scripts/autotracking/ap_helper.lua
@@ -22,11 +22,22 @@ end
 
 MAP_TOGGLE={[0]=0,[1]=1}
 MAP_TOGGLE_REVERSE={[0]=1,[1]=0}
-MAP_TRIPLE_REVERSE={[0]=2,[1]=1,[2]=0}
-MAP_FREEFLY={[0]=0,[5]=1,[6]=2,[7]=3,[8]=4,[9]=5,[10]=6,[11]=7,[12]=8,[13]=9,[15]=10}
-MAP_BADGES={}
-for i=0,8 do MAP_BADGES[i]=i end
+-- MAP_TRIPLE_REVERSE={[0]=2,[1]=1,[2]=0}
+-- MAP_FREEFLY={[0]=0,[5]=1,[6]=2,[7]=3,[8]=4,[9]=5,[10]=6,[11]=7,[12]=8,[13]=9,[15]=10}
+-- MAP_BADGES={}
+-- for i=0,8 do MAP_BADGES[i]=i end
 
 SLOT_CODES = {
-	
+	randomize_hidden_items={
+		code="hiddenitems", 
+		mapping=MAP_TOGGLE
+	},
+	goal={
+		code="goal", 
+		mapping=MAP_TOGGLE
+	},
+	require_itemfinder={
+		code="reqitemfinder", 
+		mapping=MAP_TOGGLE_REVERSE
+	}
 }
