6b2cb6ed0d71e69f350721807295ecc08122c036
アリス <alice@rootsys.sh>
2024-05-18 14:34:52 +0100

    Forgot to add stuff lol

---
+++ scripts/autotracking/flag_mapping.lua
@@ -0,0 +1,37 @@
+FLAG_EVENT_CODES = {
+	"EVENT_GOT_KENYA",
+    "EVENT_GAVE_KENYA",
+    "EVENT_JASMINE_RETURNED_TO_GYM",
+    "EVENT_DECIDED_TO_HELP_LANCE",
+    "EVENT_CLEARED_ROCKET_HIDEOUT",
+    "EVENT_CLEARED_RADIO_TOWER",
+    "EVENT_BEAT_ELITE_FOUR",
+    "EVENT_RESTORED_POWER_TO_KANTO",
+    "EVENT_VIRIDIAN_GYM_BLUE",
+    "EVENT_BEAT_RED"
+}
+
+FLAG_ITEM_CODES = {
+	{option="badges_off", codes={"ZEPHYR_BADGE"}},
+	{option="badges_off", codes={"HIVE_BADGE"}},
+	{option="badges_off", codes={"PLAIN_BADGE"}},
+	{option="badges_off", codes={"FOG_BADGE"}},
+	{option="badges_off", codes={"STORM_BADGE"}},
+	{option="badges_off", codes={"MINERAL_BADGE"}},
+	{option="badges_off", codes={"GLACIER_BADGE"}},
+	{option="badges_off", codes={"RISING_BADGE"}},
+	{option="badges_off", codes={"BOULDER_BADGE"}},
+	{option="badges_off", codes={"CASCADE_BADGE"}},
+	{option="badges_off", codes={"THUNDER_BADGE"}},
+	{option="badges_off", codes={"RAINBOW_BADGE"}},
+	{option="badges_off", codes={"SOUL_BADGE"}},
+	{option="badges_off", codes={"MARSH_BADGE"}},
+	{option="badges_off", codes={"VOLCANO_BADGE"}},
+	{option="badges_off", codes={"EARTH_BADGE"}},
+
+	{option="pokegear_off", codes={"RADIO_CARD"}},
+	{option="pokegear_off", codes=nil},
+	{option="pokegear_off", codes=nil},
+	{option="pokegear_off", codes={"EXPN_CARD"}},
+	{option="pokegear_off", codes={"POKEGEAR"}},
+}
